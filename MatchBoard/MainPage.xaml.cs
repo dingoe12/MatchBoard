2f6e75a7742bcf5039289e5fa7d0e758925d4783
Dingoe12 <thomasarendkas@hotmail.com>
2022-04-24 23:29:44 +0200

    Added basic project setup

---
+++ MatchBoard/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MatchBoard
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        int num = 1;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public void But1_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = new Button();
+            SP.Children.Add(b);
+            b.Content = "button" + num;
+            num++;
+        }
+    }
+}
